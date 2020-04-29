64d488fed31f262458c41c06ea9b632058965f06
Vadim Grom <vadim.nagor@mail.ru>
2020-04-29 11:01:46 +0300

    deleted:    docs/project.html 	modified:   main.css 	deleted:    project.html 	modified:   rybalka/index.html 	modified:   rybalka/js/main.js

---
+++ rybalka/js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        
+        items: 4,
+        loop: true,
+        dots: false, 
+        smartSpeed: 2000,
+
+
+    });
+
+
+
+  });
+
+
+ 
