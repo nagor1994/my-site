de8151b855f3f6481c52bf11e7b409da78a6fee1
Пользователь <vareniys@mail.ru>
2021-12-08 18:47:00 +0200

    rybalka change

--- rybalka/js/main.js
+++ rybalka/js/main.js
@@ -1,17 +1,29 @@
 $(document).ready(function(){
     $(".owl-carousel").owlCarousel({
         
-        items: 4,
+        items: 1,
         loop: true,
-        dots: false, 
+        dots:true, 
         smartSpeed: 2000,
 
 
     });
-
-
-
   });
 
 
- 
+  $(document).ready(function(){
+    $('.header__burger').click(function(event){
+        $('.header__burger, .header__menu').toggleClass('active');
+        $('body').toggleClass('lock');
+    });
+}); 
+
+$(document).ready(function(){
+    $('.block__title').click(function(event){
+        if($('.block').hasClass('one')){
+            $('.block__title').not($(this)).removeClass('active');
+            $('.block__text').not($(this).next()).slideUp('active');
+        }
+        $(this).toggleClass('active').next().slideToggle(300);
+    });
+}); 
