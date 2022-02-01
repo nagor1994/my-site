5a117881eb2985b3b518d426d7aebdd71de736ba
Пользователь <vareniys@mail.ru>
2022-02-01 15:28:29 +0200

    carousel rybalka

--- rybalka/js/main.js
+++ rybalka/js/main.js
@@ -1,10 +1,47 @@
 $(document).ready(function(){
-    $(".owl-carousel").owlCarousel({
+    $(".slider-one").owlCarousel({
         
         items: 1,
         loop: true,
         dots:true, 
         smartSpeed: 2000,
+        margin:30
+
+
+    });
+  });
+
+  const nextIcon = '<img src="./img/sales/right-arrow.png" alt:"right">';
+  const prevIcon = '<img src="./img/sales/left-arrow.png" alt:"left">';
+  $(document).ready(function(){
+    $(".slide-two").owlCarousel({
+        items: 4,
+        loop: true,
+        nav: true,
+        navText: [
+            prevIcon,
+            nextIcon,
+        ], 
+        smartSpeed: 2000,
+        dots:false,
+        responsive:{
+            0:{
+                items:1,
+            },
+            400:{
+                items:1,
+                margin:30,
+                
+            },
+            850:{
+                items:3,
+                margin:100,
+            },
+            1170:{
+                items:4,
+                margin:20,
+            }
+        }
 
 
     });
